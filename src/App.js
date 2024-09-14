29592ecf5bcd4f13b5bccaff852c84aed463f21b
memoldrem <madeline.moldrem@gmail.com>
2024-09-14 09:50:47 -0400

    recording button with purple start.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import React from 'react';
+import Recording_Button from './components/RecordingButton';
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <Recording_Button />
         <a
           className="App-link"
           href="https://reactjs.org"
